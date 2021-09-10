a314c0cf8d2594f5a93d84eee5b49209503027f0
Kyle <kyle.e.cardwell@gmail.com>
2021-09-09 20:10:32 -0600

    started basic server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("I'm here!")
+require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+server.use('*', (req, res) => {
+    res.json({
+        message: "this is cool!"
+    })
+})
+
+server.listen(5000, () => {
+    console.log(`listening on port ${5000}`)
+})
